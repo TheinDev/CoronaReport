55e693f2a208d16f99e5e9b90c6cc8b2dd5fadc5
Mikde <mikeethan2018@gmail.com>
2020-06-04 13:22:36 +0630

    new project is created

---
+++ corona_report/android/app/src/main/kotlin/com/theindv/corona_report/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.theindv.corona_report
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ corona_report/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ReportApp());
+}
+
+class ReportApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.green ),
+
+
+    );
+  }
+
+
+
+}
